d0cdefa09627c8ea690268ecdf27c0c5bab7a21d
Subhash Yadav <subhass7658@gmail.com>
2024-08-06 17:54:12 +0000

    Different types of variables with output

--- Basic_01/variables.java
+++ Basic_01/variables.java
@@ -8,9 +8,42 @@
 🔴 boolean - stores values with two states: true or false
  */
 
+/*
+👉 *************Declaring (Creating) Variables*****************
 
+ Syntax:- type variableName = value;
 
-public class variables {
-    
-    
+ */
+
+class variables {
+
+    public static void main(String[] args) {
+
+        // writing variables string
+
+        String name = "Subhash";
+        System.out.println(name);
+
+        // Integer Variables ⤵️
+        int myNum = 15;
+        System.out.println(myNum);
+
+        // Write another way on integer
+        int mynumber;
+        mynumber = 20;
+        System.out.println(mynumber);
+
+        // Other types of variables
+
+        float myFloatNum = 5.99f;
+        System.out.println(myFloatNum);
+
+        char myLetter = 'D';
+        System.out.println(myLetter);
+
+        boolean myBool = true;
+        System.out.println(myBool);
+        
+    }
+
 }
