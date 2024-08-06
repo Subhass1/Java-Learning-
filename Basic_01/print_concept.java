43ee4b28f5de45983413f12df1f20ec01d9174a2
Subhash Yadav <subhass7658@gmail.com>
2024-08-06 16:54:36 +0000

    Print output concept

---
+++ Basic_01/print_concept.java
@@ -0,0 +1,28 @@
+/*  Java learning Concept 
+ How to print the content */
+
+// 👉 For examples:- 
+
+class java {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        System.out.println("My name is subhash Yadav");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
+    }
+}
+
+
+/*✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️ Notes ✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️
+❗imp println() is a method.
+
+***************Double Quotes***************
+● System.out.println("This sentence will work!");✅
+● System.out.println(This sentence will produce an error);❌
+
+#_There is also a print() method, which is similar to println().
+{ The main difference is that both can print the output but println can print the output in new line.}
+ ●They both are methods.
+
+*/
