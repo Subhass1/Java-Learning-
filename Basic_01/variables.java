48f17238db4248458283e22b2e71161c3ef58045
Subhash Yadav <subhass7658@gmail.com>
2024-08-06 17:00:12 +0000

    Types of variables

---
+++ Basic_01/variables.java
@@ -0,0 +1,16 @@
+/*✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️ Variables_Concept ✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️
+ In Java, there are different types of variables, for example:
+
+🔴 String - stores text, such as "Hello". String values are surrounded by double quotes
+🔴 int - stores integers (whole numbers), without decimals, such as 123 or -123
+🔴 float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+🔴 char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+🔴 boolean - stores values with two states: true or false
+ */
+
+
+
+public class variables {
+    
+    
+}
