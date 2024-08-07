15ddbe226a8ba73cdea239bdbf0413672aff6904
Subhash Yadav <subhass7658@gmail.com>
2024-08-07 02:25:31 +0000

    Datatypes_concept

---
+++ Basic_01/data-types.java
@@ -0,0 +1,27 @@
+/*
+✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️ Datatypes_Concept ✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️
+
+👉Data types are divided into two groups:
+         ● Primitive data types - includes byte, short, int, long, float, double, boolean and char
+         ● Non-primitive data types - such as String, Arrays and Classes 
+
+         Datatypes                      Size
+         byte ========================> 1 byte
+         short =======================> 2 bytes
+         long ========================> 8 bytes
+         int =========================> 4 bytes 
+         char ========================> 2 byte
+         float =======================> 4 bytes
+         double ======================> 8 bytes
+         boolean =====================> 1 bit
+
+         👉Non-Primitive Data Types
+         ● Non-primitive data types are called reference types because they refer to objects.
+
+         ● The main difference between primitive and non-primitive data types are:
+
+         ● Primitive types are predefined (already defined) in Java. Non-primitive types are created by the programmer and is not defined by Java (except for String).
+         ● Non-primitive types can be used to call methods to perform certain operations, while primitive types cannot.
+         ● A primitive type always has a value, while non-primitive types can be null.
+         ● A primitive type starts with a lowercase letter, while non-primitive types starts with an uppercase letter.
+*/
