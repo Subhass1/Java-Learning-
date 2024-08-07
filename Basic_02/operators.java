20f5216fc6b4bfaa32501bbbdcdc3434c60e2d39
Subhash Yadav <subhass7658@gmail.com>
2024-08-07 14:09:31 +0000

    Learn operators in java

--- Basic_02/operators.java
+++ Basic_02/operators.java
@@ -1,3 +1,71 @@
+/* 
+✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️ Type Casting_Concept ✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️
+🔴Java divides the operators into the following groups:
+            ● Arithmetic operators
+            ● Assignment operators
+            ● Comparison operators
+            ● Logical operators
+            ● Bitwise operators
+*/
+
+//           👉Arithmetic Operators
+/*
+  Arithmetic operators are used to perform common mathematical operations.
+
+Operator  Name	                    Description	                          Example	
++ 	      Addition	            Adds together two values	               x + y	
+-	      Subtraction	        Subtracts one value from another	       x - y	
+*	      Multiplication	    Multiplies two values	                   x * y	
+/	      Division	            Divides one value by another	           x / y	
+%	      Modulus	            Returns the division remainder	           x % y	
+++     	  Increment	            Increases the value of a variable by 1	   ++x	
+--	      Decrement	            Decreases the value of a variable by 1	   --x	
+ */
+
 public class operators {
     
 }
+
+//     👉Java Assignment Operators
+/*  The addition assignment operator (+=) adds a value to a variable:
+
+Operator	Example	       Same As	
+=	        x = 5	       x = 5	
++=	        x += 3	       x = x + 3	
+-=	        x -= 3	       x = x - 3	
+*=	        x *= 3	       x = x * 3	
+/=	        x /= 3	       x = x / 3	
+%=	        x %= 3	       x = x % 3	
+&=	        x &= 3	       x = x & 3	
+|=	        x |= 3	       x = x | 3	
+^=	        x ^= 3	       x = x ^ 3	
+>>=	        x >>= 3	       x = x >> 3	
+<<=	        x <<= 3	       x = x << 3
+ 
+ */
+
+
+
+ // 👉Java Comparison Operators
+ /* Comparison operators are used to compare two values (or variables). This is important in programming, because it helps us to find answers and make decisions.
+     Operator	Name	                      Example	
+    ==	        Equal to	                   x == y	
+    !=	        Not equal	                   x != y	
+    >	        Greater than	               x > y	
+    <	        Less than	                   x < y	
+    >=	        Greater than or equal to	   x >= y	
+    <=	        Less than or equal to	       x <= y
+  */
+
+
+
+
+
+  //  👉Java Logical Operators
+  /* 🔴Logical operators are used to determine the logic between variables or values:
+  Operator	  Name	           Description	                                              Example
+   && 	    Logical and     Returns true if both statements are true	                  x < 5 &&  x < 10	
+   || 	    Logical or	    Returns true if one of the statements is true	              x < 5 || x < 4	
+   !	    Logical not	    Reverse the result, returns false if the result is true	      !(x < 5 && x < 10)
+   
+   */
