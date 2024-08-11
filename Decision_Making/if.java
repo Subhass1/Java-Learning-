bd03a99e7c24d5f07a379c2be2b6e26bcad2ceb7
Subhass1 <subhass7658@gmail.com>
2024-08-11 22:15:12 +0545

    If else statement learn

--- Decision_Making/if.java
+++ Decision_Making/if.java
@@ -2,7 +2,19 @@ package Decision_Making;
 
  class if1 {
     public static void main(String [] args){
+        if (20 > 18) {
+            System.out.println("20 is greater than 18");
+          }
 
     }
     
 }
+
+// 🔴If Statement 
+/*   ● Use the if statement to specify a block of Java code to be executed if a condition is true.
+     ● if (condition) {
+                // block of code to be executed if the condition is true
+         }
+     ●Note that if is in lowercase letters. Uppercase letters (If or IF) will generate an error.
+ 
+ */
