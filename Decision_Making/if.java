85264f2ac78fe5a6da178c3e273f4c0e67d7dad7
Subhass1 <subhass7658@gmail.com>
2024-08-11 22:18:16 +0545

    Changes

--- Decision_Making/if.java
+++ Decision_Making/if.java
@@ -2,7 +2,7 @@ package Decision_Making;
 
  class if1 {
     public static void main(String [] args){
-        if (20 > 18) {
+        if (20 > 15) {
             System.out.println("20 is greater than 18");
           }
 
@@ -15,6 +15,6 @@ package Decision_Making;
      ● if (condition) {
                 // block of code to be executed if the condition is true
          }
-     ●Note that if is in lowercase letters. Uppercase letters (If or IF) will generate an error.
+     ● Note that if is in lowercase letters. Uppercase letters (If or IF) will generate an error.
  
  */
