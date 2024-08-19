cfc50ec90ce70cc7283eb0166fc76410c559e3aa
Subhass1 <subhass7658@gmail.com>
2024-08-19 22:31:19 +0545

    Comments of java

---
+++ Basic_01/comments.java
@@ -0,0 +1,13 @@
+/** ⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉ About comments of java ⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉
+    🔴Question:- Whata s comments? full explanatioin.
+              ⇉ Comments are the statements that are totally igonered by the compiler and interpreter.
+
+             ✍️ Why we use comments?
+             ⇉ i) The comments can be used to provide explanation about the program.
+               ii) Comments makes program move understandable and readable to the others.
+                    ➡️ It's types:
+                      1) Single line comment //
+                      2) Multi line comment  
+                      3) Documentation comment 
+
+ */
