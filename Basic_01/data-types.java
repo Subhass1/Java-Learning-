0f8a3e604f5eea52160290df15fa4ac37cbf0b2c
Subhass1 <subhass7658@gmail.com>
2024-08-19 23:29:26 +0545

    Updates in datatypes

--- Basic_01/data-types.java
+++ Basic_01/data-types.java
@@ -1,27 +1,54 @@
-/*
-✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️ Datatypes_Concept ✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️✍️
+/* ⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉ Datatypes_Concept in java ⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉
+    ✍️ Definition: Datatype specify the different sizes and values that can be stored in the variables.
 
-👉Data types are divided into two groups:
-         ● Primitive data types - includes byte, short, int, long, float, double, boolean and char
-         ● Non-primitive data types - such as String, Arrays and Classes 
+
+👉 Data types are divided into two groups:
+         ● Primitive data types are ⤵️ 
+                      //\\
+                     //   \\
+                    //     \\
+                   //        \\
+                  //           \\
+                 //               \\
+             Numeric             Non-numeric
+                                   ● Char (2 byte)
+                                   ● Boolean (1 bit) True or False
+                //
+               // \\
+              //   \\
+             //     \\
+            //       \\
+        Integric     Decimal
+ ● Byte (1 byte)      ● Float (4 byte)    
+ ● Short (2 byte)     ● Double (8 byte)
+ ● int (2 byte)
+ ● Long (8 byte)
 
          Datatypes                      Size
-         byte ========================> 1 byte
-         short =======================> 2 bytes
-         long ========================> 8 bytes
-         int =========================> 4 bytes 
-         char ========================> 2 byte
-         float =======================> 4 bytes
-         double ======================> 8 bytes
-         boolean =====================> 1 bit
+         byte    ‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗ 1 byte
+         short   ‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗ 2 bytes
+         long    ‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗ 8 bytes
+         int     ‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗ 4 bytes 
+         char    ‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗ 2 byte
+         float   ‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗ 4 bytes
+         double  ‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗ 8 bytes
+         boolean ‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗ 1 bit
+ 
 
-         👉Non-Primitive Data Types
-         ● Non-primitive data types are called reference types because they refer to objects.
 
-         ● The main difference between primitive and non-primitive data types are:
 
-         ● Primitive types are predefined (already defined) in Java. Non-primitive types are created by the programmer and is not defined by Java (except for String).
-         ● Non-primitive types can be used to call methods to perform certain operations, while primitive types cannot.
-         ● A primitive type always has a value, while non-primitive types can be null.
-         ● A primitive type starts with a lowercase letter, while non-primitive types starts with an uppercase letter.
+        🔴 Non-primitive data types ⤵️
+              ● String
+              ● Arrays 
+              ● Classes
+              ● Interface
+
+    ⇶ Non-primitive data types are called reference types because they refer to objects.
+
+    👉The main difference between primitive and non-primitive data types are:
+            ⇉ Primitive types are predefined (already defined) in Java. Non-primitive types are created by the programmer and is not defined by Java (except for String).
+            ⇉ Non-primitive types can be used to call methods to perform certain operations, while primitive types cannot.
+            ⇉ A primitive type always has a value, while non-primitive types can be null.
+            ⇉ A primitive type starts with a lowercase letter, while non-primitive types starts with an uppercase letter.
+  
 */
