58c48f61be4072c218585a41748a5d1f44d30aa0
Subhass1 <subhass7658@gmail.com>
2024-08-19 21:45:45 +0545

    Learn about java history

---
+++ Basic_01/About_Java.java
@@ -0,0 +1,80 @@
+/* ⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉  👉 History of Java ⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉ 
+
+ 🔴JDK:- JDK stands for java development kit, it internally contains JRE and JVM where JRE stands for java runtime environment 
+         & JVM stands for java virtual machine. JDK provides all the tools to work with java language.
+
+ 🔴 JRE:- JRE stands for java runtime environment to execute the java program. 
+          It internally contains JVM which is responsible to execute a java program. 
+          
+ 🔴 JVM:- JVM stands for java vortual machine. It is the software in the form of interpreter 
+          which has written in c language through which we execute our java program.
+          
+           ➡️  JDK: JRE + JVM (Library) 
+           
+*/
+
+/** ⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉ ✍️What is java? With syntax and example.⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉ 
+ 
+  —————→ Java is class based high level object oriented programming language developed by "James Gosling" and his friends in the year 1991.
+
+ ➡️The first version of java (JDK 1.0) was released on the year 23rd jan 1996 by sun microsystem.
+
+ 🔴Syntax:- Class class-name{
+                 public static void main (String [] args){
+
+                 System.out.print(" ");
+               }
+            }
+
+            👉Rule:- i) Save ⇉ Class-name.java
+                     ii) Compilation ⇉ java c class-name
+                     iii) Execution ⇉ java class-name
+ */
+
+ /** ⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉ ✍️Java History ⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉ 
+  
+  ●Java is a totally computer based programming language developed by sun microsystem (James Gosling, Mike sheridan and patricknaughton).
+  ● In the year 1991 James Gosling and his friends start a project.  
+              Team ⇒ Green Team
+
+ ● James Gosling —————→ green talk 
+    Extension —————→ .gt
+
+ ● green talk name changed to oak. Oak changed to i) National tree (U.S.A, france etc) ii) oak technology
+  
+ ● Oak —————→ java (1995)
+ ● Java developed for sethbox, television, remote etc
+ ● java become popular on internet programming 
+ ● Version —————→ JDK alpha and Beta 1995 (Bought By sun microsystem)
+ ● latest version : ...JDK
+ ● Sun microsystem —————→ sold to oracle corporation, 2010
+
+ 🔴Java Category⤵️
+     i) Core Java 
+     ii) Advance java
+     iii) Android java
+
+
+     👉 Features of java⤵️
+     i) Object oriented 
+     ii) Platform independent 
+     iii) Simple language 
+     iv) secure
+     v) portable 
+     vi) Conmpiled and interpreted
+     vii) Robust
+     viii) Distributed 
+     ix) Multi-threaded
+     x) Performance
+     xi) Dynamic
+  
+     
+     ⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉ Some Question ⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉ 
+     🔴Why java so popular? 
+ */
+
+         
+
+ public class About {
+
+ }
