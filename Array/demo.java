de0b5ceb73293cab0b13b7c29c7808352392f96c
Subhass1 <subhass7658@gmail.com>
2024-09-02 23:33:26 +0545

    updates

---
+++ Array/demo.java
@@ -0,0 +1,23 @@
+/* ⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉  👉 All about java methods  ⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉⇉ 
+🔴Java Arrays.compare() Method
+Syntax: compare()                   »→ Compares between two arrays
+
+● Some imp information:
+            ⇉ Returns 0 if the arrays are equal.
+            ⇉ Returns a negative integer if the array1 is less than array2 lexicographically
+            ⇉ Returns a positive integer if array1 is greater than array2 lexicographically.
+            
+*/
+
+// For examples: 
+package Array;
+
+public class demo {
+
+    public static void main(String [] args){
+        String [] A = {"Java", "Desktop", "Pen", "Pencil"};
+        String [] B = {"Java", "Desktop", "Pen", "Pencil"};
+
+        System.out.println("Comparing Arrays: " + Arrays.compare(A,B));
+    }
+}
